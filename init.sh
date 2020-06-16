35f4862b7c644803c9cfad9e321af9e3198b5929
Valentin Volkl <valentin.volkl@cern.ch>
2020-06-16 18:31:49 +0200

    [ci] remove obsolete workaround for lcg releases

--- init.sh
+++ init.sh
@@ -1,7 +1,3 @@
 
 source /cvmfs/sw-nightlies.hsf.org/key4hep/views/latest/x86_64-centos7-gcc8-opt/setup.sh
 
-# workaround for a bug in the Gaudi CMake configuration,
-# can be removed in the future
-export Gaudi_DIR=/cvmfs/sft.cern.ch/lcg/releases/LCG_96c_LS/Gaudi/v32r2/x86_64-centos7-gcc8-opt/
-
